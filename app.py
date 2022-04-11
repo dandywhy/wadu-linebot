0eac0ce09d196471c752d1b57baa14432ebd897e
Dandy <lavenderflyyaya@gmail.com>
2022-04-11 17:51:10 +0800

    push heroku

--- app.py
+++ app.py
@@ -303,7 +303,7 @@ def handle_message(event):
         message = TextSendMessage(text='資料已清空!')
         line_bot_api.reply_message(event.reply_token, message)
     elif msg == '私聊':
-        message = 'https://liff.line.me/1657027110-vXaP6gjP'
+        message = TextSendMessage(text='https://liff.line.me/1657027110-vXaP6gjP')
         line_bot_api.reply_message(event.reply_token, message)
     if msg[2:4] == "天氣":
         station = msg[:2]
